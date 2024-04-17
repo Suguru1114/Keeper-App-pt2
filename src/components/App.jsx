43938df78c7327b66169b323ad26f8c1a79a5029
Suguru Miyata <suguru1114@gmail.com>
2024-04-17 16:51:33 +1000

    add props for notes & ready for adding map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,26 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+console.log(notes);
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {/* add title & content in note */}
+      <Note 
+        title="This is the note title"
+        content="This is the note content"
+      />
       <Footer />
     </div>
   );
 }
+
+//1. create props for each card
+//2. use arrow function
+//3. 
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-function Note() {
+//1.add props in Note()
+function Note(props) {
   return (
     <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
+
+    {/* 2. add{props.title} */}
+
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
