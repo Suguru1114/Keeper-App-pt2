c9b852d6ce558c87b83ab1c57de19fbd1877e34a
Suguru Miyata <suguru1114@gmail.com>
2024-04-19 13:58:10 +1000

    add props and adding createNote function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,29 @@ import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
 
-console.log(notes);
+// console.log(notes); to check if the notes from notes is passed or not
+
+function createNote(notesItem) {
+  return <Note 
+      title = {notes.title}
+      content = {notes.content}
+  />
+}
 
 function App() {
   return (
     <div>
       <Header />
-      {/* add title & content in note */}
-      <Note 
-        title="This is the note title"
+      {/* 2. add title & content in note */}
+      {/* 4.change title & content for using map function to Loop
+      from notes */}
+       
+        {/* title="This is the note title"
         content="This is the note content"
-      />
+        create map function here
+        abd pass the notrItem function here */}
+        {notes.map(createNotes)}
+      
       <Footer />
     </div>
   );
